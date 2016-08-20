4daaa7e7c09651e350bc200228f68cc435c7979b
jm-rives <joanna.rives@gmail.com>
2016-08-20 15:19:55 -0700

    ADD dead_bug method

--- ADA_Homework_week_2/GuessWordGame-master/art.rb
+++ ADA_Homework_week_2/GuessWordGame-master/art.rb
@@ -1,48 +1,57 @@
-# bug_base = """
+# bug_base = "
 
 #  ---- \\----//
 # /    \/ @ @ \\n
 # L----L\__0__/
 
-# """
-# bug_butt = """
+# "
+# bug_butt = "
 #  ----  
 # /    \\
 # l    l
 
-# """
+# "
 # bug = bug_base
 # puts bug_butt + bug_base
 
 # starts at initialization of class
-def build_base_bug
-  puts bug_line_1 = " ---- \\----//"
+def basic_bug
+  puts bug_line_1 = " ----\\----/"
   puts bug_line_2 = "/    \/ @ @ \\"
-  puts bug_line_3 = "l====L\__0__/"
+  puts bug_line_3 = "l====L\__v__/"
+end
+
+def dead_bug
+  puts bug_line_1 = " ----\\----/"
+  puts bug_line_2 = "/    \/ x x \\"
+  puts bug_line_3 = "l====L\__^__/"
 end
 
 # puts bug_line_1
 # puts bug_line_2
 # puts bug_line_3
 
-# puts build_base_bug # print debuggin' # werks
+# puts build_base_bug # print debuggin" # werks
 # added after each won round
 
 # TODO jm-rives l/u and utilize ruby raw strings
 
-# one to be added after each successful round
-# def build_bug_segment
-#   puts extend_bug_line_1 = " ---- "
-#   puts extend_bug_line_2 = "/    \\"
-#   puts extend_bug_line_3 = "l====l"
-# end
+# one to be added after each correct guess
 
-def build_bug_segment(win)
-  win.times do
+
+def bug_segment(win, default=0)
+  reward = win - default
+  reward.times do
   puts extend_bug_line_1 = " ---- "
   puts extend_bug_line_2 = "/    \\"
   puts extend_bug_line_3 = "l====l"
   end
 end
 
-build_base_bug(3)
+#begin testing 
+# dead_bug
+# bug_segment(2)
+
+# basic_bug
+# bug_segment(2)
+
